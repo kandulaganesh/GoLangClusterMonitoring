c99392a4a0e1d4501b2b9c1f9fe4d06887419f1d
Ganesh kandula <g.kandula@f5.com>
2023-06-13 08:36:13 -0700

    fix: space issue

--- streamPodLogs/streamPodLogs.go
+++ streamPodLogs/streamPodLogs.go
@@ -13,6 +13,7 @@ import (
 
 func CapturePodLogs(w http.ResponseWriter, r *http.Request, podName string, namespace string) {
     // Specify the kubeconfig file path
+    // TODO: Comeback on this
     kubeconfigPath := "/root/dev_volume/golang_crash_course/k3s.yaml"
 
     // Build the configuration from the provided kubeconfig file
@@ -33,37 +34,37 @@ func CapturePodLogs(w http.ResponseWriter, r *http.Request, podName string, name
         return
     }
 
-        w.Header().Set("Content-Type", "text/event-stream")
-        w.Header().Set("Cache-Control", "no-cache")
-        w.Header().Set("Connection", "keep-alive")
-        w.Header().Set("Transfer-Encoding", "chunked")
+    w.Header().Set("Content-Type", "text/event-stream")
+    w.Header().Set("Cache-Control", "no-cache")
+    w.Header().Set("Connection", "keep-alive")
+    w.Header().Set("Transfer-Encoding", "chunked")
 
-        // Retrieve the live logs of the pod
-        stream, err := clientset.CoreV1().Pods(namespace).GetLogs(podName, &corev1.PodLogOptions{
-            Follow: true,
-        }).Stream(r.Context())
+    // Retrieve the live logs of the pod
+    stream, err := clientset.CoreV1().Pods(namespace).GetLogs(podName, &corev1.PodLogOptions{
+        Follow: true,
+    }).Stream(r.Context())
+    if err != nil {
+        log.Printf("Error retrieving pod logs: %v", err)
+        http.Error(w, "Error retrieving logs", http.StatusInternalServerError)
+        return
+    }
+    defer stream.Close()
+
+    buf := make([]byte, 4096)
+    for {
+        bytesRead, err := stream.Read(buf)
         if err != nil {
-            log.Printf("Error retrieving pod logs: %v", err)
-            http.Error(w, "Error retrieving logs", http.StatusInternalServerError)
+            log.Printf("Error reading pod logs: %v", err)
             return
         }
-        defer stream.Close()
 
-        buf := make([]byte, 4096)
-        for {
-            bytesRead, err := stream.Read(buf)
-            if err != nil {
-                log.Printf("Error reading pod logs: %v", err)
-                return
-            }
-
-            if bytesRead > 0 {
-                fmt.Fprintf(w, "data: %s", string(buf[:bytesRead]))
-                flusher.Flush()
-            }
-
-            // Add a small delay to control the streaming rate
-            time.Sleep(100 * time.Millisecond)
+        if bytesRead > 0 {
+            fmt.Fprintf(w, "data: %s", string(buf[:bytesRead]))
+            flusher.Flush()
         }
+
+        // Add a small delay to control the streaming rate
+        time.Sleep(100 * time.Millisecond)
+    }
 }
 
